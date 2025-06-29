73d56ebe972b5f84ffe347ed50cfe33f5cb0d9d7
akashdeep-singh-2696 <g24ai1071@iitj.ac.in>
2025-06-29 06:05:06 +0000

    Refactored file and utils added

---
+++ plot_digits_classification_refactored.py
@@ -0,0 +1,24 @@
+from utils import (
+    load_digits_data, plot_training_images, flatten_images, split_data,
+    train_classifier, predict, plot_predictions, print_classification_report,
+    plot_confusion_matrix, rebuild_classification_report_from_cm,
+    show_plots, tune_hyperparameters  # Added tune_hyperparameters
+)
+
+def main():
+    digits = load_digits_data()
+    plot_training_images(digits)
+    data = flatten_images(digits)
+    X_train, X_test, y_train, y_test = split_data(data, digits.target)
+    # Use hyperparameter tuning instead of fixed training
+    clf = tune_hyperparameters(X_train, y_train)
+
+    predicted = predict(clf, X_test)
+    plot_predictions(X_test, predicted)
+    print_classification_report(y_test, predicted, clf)
+    disp = plot_confusion_matrix(y_test, predicted)
+    rebuild_classification_report_from_cm(disp)
+    show_plots()
+
+if _name_ == "_main_":
+    main()
